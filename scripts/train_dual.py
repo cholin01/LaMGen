483603408bcddb60fee8024d1defdd001b1fa577
cholin01 <152848794+cholin01@users.noreply.github.com>
2026-02-27 22:19:10 +0800

    Update train_dual.py

--- scripts/train_dual.py
+++ scripts/train_dual.py
@@ -21,12 +21,12 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
 from datetime import timedelta
 
-sys.path.append(os.path.dirname((os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.early_stop.pytorchtools import EarlyStopping
 from model.lamgen_model import LaMGen_dual
 from utils.bert_tokenizer import ExpressionBertTokenizer
 
-abs_path = os.path.dirname((os.path.dirname(os.path.abspath(__file__)))
+abs_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 br_re = re.compile('Br')
 cl_re = re.compile('Cl')
